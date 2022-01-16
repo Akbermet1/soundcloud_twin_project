5ff51d595900c78643ef06dd34363f2e17c19d09
Akbermet Zhuma <akbermet.zh@gmail.com>
2022-01-16 21:24:29 +0600

    pagination works properly

--- main/views.py
+++ main/views.py
@@ -6,6 +6,8 @@ from .serializers import GenreSerializer, AudioSerializer
 from rest_framework import status
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
+from rest_framework.mixins import ListModelMixin
+from rest_framework.pagination import PageNumberPagination
 
 
 User = get_user_model()
@@ -19,14 +21,37 @@ class GenreListCreateView(ListCreateAPIView):
 class AudioViewSet(viewsets.ViewSet):
     serializer_class  = AudioSerializer
     queryset = Audio.objects.all()
+    pagination_class = PageNumberPagination
+    # lookup_field = 'email'
 
     def list(self, request):
         queryset = Audio.objects.filter(visibility='Public')
-        serializer  = AudioSerializer(queryset, many=True)
-        return Response(serializer.data)
+        pagination = PageNumberPagination()
+        qs = pagination.paginate_queryset(queryset, request)
+        serializer  = AudioSerializer(qs, many=True)
+        return pagination.get_paginated_response(serializer.data)
+        # return Response(serializer.data)
 
-    def retrieve(self, request, pk):
+    def retrieve(self, request, pk:str):
         user = get_object_or_404(User, email=pk)
         queryset = user.audios
         serializer = AudioSerializer(queryset, many=True)
         return Response(serializer.data)
+
+
+# class AudioViewSet(viewsets.ModelViewSet):
+#     serializer_class  = AudioSerializer
+#     queryset = Audio.objects.all()
+#     # lookup_field = 'email'
+#     pagination_class = PageNumberPagination
+
+#     def list(self, request):
+#         queryset = Audio.objects.filter(visibility='Public')
+#         serializer  = AudioSerializer(queryset, many=True)
+#         return Response(serializer.data)
+
+#     def retrieve(self, request, pk:str):
+#         user = get_object_or_404(User, email=pk)
+#         queryset = user.audios
+#         serializer = AudioSerializer(queryset, many=True)
+#         return Response(serializer.data)
